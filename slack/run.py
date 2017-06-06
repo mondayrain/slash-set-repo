c55fb28efdef4eb1aba6a9d6a8c027f2e2938b44
Larissa Feng <hello@larissafeng.me>
2017-06-06 00:00:58 -0400

    Fully working routes

--- slack/run.py
+++ slack/run.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, abort
+import json
 
 # The parameters included in a slash command request (with example values):
 #   token=gIkuvaNzQIHg97ATvDxqgjtO
@@ -22,50 +23,62 @@ class Repo(object):
 
 app = Flask(__name__)
 
-@app.route('/set-repo', methods=['POST'])
+@app.route('/set-repo-message', methods=['POST'])
 def set_repo():
-    params = parse_params(request)
-    if not params['token']:  # or some other failure condition
+    params = _parse_params(request)
+    if not params['token']:
         abort(400)
 
-    # TODO: Handle it if wrong # of arguments sent
-    repo_name, message = tuple(params["text"].split(' ', 1))
+    if len(params['text'].split(' ')) <= 1:
+        return "ERROR: You need to provide a message to set for repository <{}>".format(params["text"])
+    else:
+        repo_name, message = params['text'].split(' ', 1)
 
-    if set_repos.get(repo_name):
-        return "Repository <{}> has already been set by {} with the message '{}'. Please first unset it.".format(
+        if set_repos.get(repo_name):
+            return "Repository <{}> has already been set by {} with the message '{}'. Please first unset it.".format(
             repo_name,
             set_repos[repo_name].user,
             set_repos[repo_name].message
             )
-    else:
-        new_repo = Repo(repo_name, message, params['user'])
-        set_repos[repo_name] = new_repo
-        return "SUCCESS! Repository <{}> has been set with the message '{}'".format(repo_name, message)
+        else:
+            new_repo = Repo(repo_name, message, params['user'])
+            set_repos[repo_name] = new_repo
+            return "SUCCESS! Repository <{}> has been set with the message '{}'.".format(repo_name, message)
 
-@app.route('/unset-repo', methods=['POST'])
+@app.route('/unset-repo-message', methods=['POST'])
 def unset_repo():
-    params = parse_params(request)
+    params = _parse_params(request)
     if not params['token']:
         abort(400)
 
-    repo_name = params["text"]
-    repo = set_repos.pop(repo_name, None)
-
-    if repo:
-        return "SUCCESS! Repository <{}> has been unset".format(repo_name)
+    if len(params['text'].split(' ')) > 1:
+        return "ERROR: Please only enter the name of the repository."
     else:
-        return "Nothing happened: repository <{}> wasn't set in the first place!".format(repo_name)
+        repo_name = params['text']
+        repo = set_repos.pop(repo_name, None)
 
-@app.route('/get-repos', methods=['POST'])
+        if repo:
+            return "SUCCESS! Message '{}' for repository <{}> has been unset".format(repo.message, repo_name)
+        else:
+            return "Nothing happened: repository <{}> wasn't set in the first place!".format(repo_name)
+
+@app.route('/get-repo-messages', methods=['POST'])
 def get_repos():
     response = "Currently set repositories: \n\n" 
     for name, repo in set_repos.items():
         response = response + "<{}>: '{}'\n".format(name, repo.message)
 
-    print(response)
     return response
 
-def parse_params(request):
+@app.route('/get-repo-messages-json', methods=['POST'])
+def get_repos_json():
+    repos = {}
+    for name, repo in set_repos.items():
+        repos[name] = { 'message': repo.message, 'user': repo.user}
+
+    return json.dumps(repos)
+
+def _parse_params(request):
     token = request.form.get('token', None)
     command = request.form.get('command', None)
     text = request.form.get('text', None)
